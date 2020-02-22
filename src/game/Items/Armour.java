68e3356dfc2f8894aae4c03dea09630c4e3a6faf
dod98 <dod98@10.152.0.28>
2020-02-22 18:06:42 +0000

    move into folder for items

---
+++ src/game/Items/Armour.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.Items;
+
+import game.Items.Item;
+
+/**
+ *
+ * @author dod98
+ */
+public class Armour extends Item {
+    int def;
+    int type;
+    boolean equipped;
+    
+    public Armour(String name, String desc,int def, int type ) {
+        super(name, desc);
+        this.def =def;
+        this.type=type;
+    }
+
+    public boolean isEquipped() {
+        return equipped;
+    }
+
+    public void setEquipped(boolean equipped) {
+        this.equipped = equipped;
+    }
+
+    public int getType() {
+        return type;
+    }
+    
+    
+    
+    public int getDef() {
+        return def;
+    }
+
+    
+}
---
+++ src/game/Items/Weapon.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.Items;
+
+import game.Items.Item;
+
+/**
+ *
+ * @author dod98
+ */
+public class Weapon  extends Item{
+    
+    int dmg;
+    boolean equipped;
+    int type;
+ 
+    public Weapon(String name, String desc, int dmg, int type) {
+        super(name, desc);
+        this.dmg =dmg;
+        this.type =type;
+    }
+
+    public int getDmg() {
+        return dmg;
+    }
+
+    public void setDmg(int dmg) {
+        this.dmg = dmg;
+    }
+
+    public void setEquipped(boolean equipped) {
+        this.equipped = equipped;
+    }
+
+    public boolean isEquipped() {
+        return equipped;
+    }
+
+    public int getType() {
+        return type;
+    }
+    
+    
+    
+    
+    
+    
+}
